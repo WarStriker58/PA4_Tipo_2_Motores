6eceefeff3a91df2c8148b9c8c0fd26b2b414d49
Sebastian Andre Salas Córdova <sebastian.salas@tecsup.edu.pe>
2024-06-20 16:39:17 -0500

    Bases for the prototype

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    public Rigidbody rb;
+    private bool isGrounded;
+    private Vector2 movementInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = transform.right * movementInput.x + transform.forward * movementInput.y;
+        Vector3 velocity = moveDirection * speed;
+        rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        else if (collision.gameObject.CompareTag("Death"))
+        {
+            SceneManager.LoadScene("DefeatScene");
+        }
+        else if (collision.gameObject.CompareTag("Victory"))
+        {
+            SceneManager.LoadScene("VictoryScene");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
